ac50abf96de0e342e63b1a98339989aa30f72154
Alex Chindea <alex@192-168-0-102.rdsnet.ro>
2014-02-18 08:00:49 +0200

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Alex Chindea on 18/02/14.
+//  Copyright (c) 2014 Alex Chindea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
