89014514a5ec9f9fc59300309682ec63fc1ba982
al3x3c <Alex Chindea>
2014-05-20 12:37:29 +0300

    Added Custom View Controllers

---
+++ ToDoList/XYZToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Alex Chindea on 20/05/14.
+//  Copyright (c) 2014 Alex Chindea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZToDoListTableViewController : UITableViewController
+
+@end
